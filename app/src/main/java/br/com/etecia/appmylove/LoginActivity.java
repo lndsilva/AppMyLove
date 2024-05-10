bc3a57d03362324b1f522e9cce31aa3d4cb79823
lndsilva <lndsilva@hotmail.com>
2024-05-09 21:46:52 -0300

    inserindo icones

--- app/src/main/java/br/com/etecia/appmylove/LoginActivity.java
+++ app/src/main/java/br/com/etecia/appmylove/LoginActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.appmylove;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
+    Button btnEntrar, btnSair;
+    TextInputEditText txtUsuario, txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,16 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
